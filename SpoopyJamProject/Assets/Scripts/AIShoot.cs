88b9b25fa9e6fefdb0aebe944e0f6a638bc200a3
waramsey <46686986+waramsey@users.noreply.github.com>
2019-10-19 14:57:11 -0500

    AIShoot
    bots are just nested if statements change my mind

---
+++ SpoopyJamProject/Assets/Scripts/AIShoot.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIShoot : MonoBehaviour
+{
+    public float speed;
+    private Transform target;
+    private Transform firePoint;
+    public GameObject arrow;
+    public float fireRate = 1f;
+    private float lastShot = 0.0f;
+    public float sightRadius;
+    public float tooClose;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mathf.Abs(target.position.x - transform.position.x) < tooClose)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, -(speed * Time.deltaTime));
+        }
+        if (Mathf.Abs(target.position.x - transform.position.x) < sightRadius && Mathf.Abs(target.position.y - transform.position.y) < 10)
+        {
+            Shoot();
+        }
+    }
+
+
+
+
+
+
+
+
+
+    void Shoot()
+    {
+        if (Time.time > fireRate + lastShot)
+        {
+            Instantiate(arrow, firePoint.position, firePoint.rotation);
+            lastShot = Time.time;
+        }
+    }
+}
