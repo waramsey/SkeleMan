952d24cc50b18276f6dda8b808e466739ef7c04a
waramsey <46686986+waramsey@users.noreply.github.com>
2019-10-18 23:33:37 -0500

    Toggle HP Images Commit
    Created a basic UI and wrote a script detecting collisions and decrimenting health.

---
+++ SpoopyJamProject/Assets/HealthChange.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthChange : MonoBehaviour
+{
+    public int HP;
+    public Sprite sprite1;
+    public Sprite sprite2;
+    public GameObject[] hearts;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HP = 3;
+        for (int i = 0; i < 3; i++)
+        {
+            hearts[i] = GameObject.Find("/BoneHeart1");
+            hearts[i].GetComponent<SpriteRenderer>().sprite = sprite1;
+        }
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        HP--;
+        hearts[HP].GetComponent<SpriteRenderer>().sprite = sprite2;
+    }
+}
