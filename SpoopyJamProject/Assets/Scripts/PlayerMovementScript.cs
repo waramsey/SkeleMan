54b640e497167632e6081c97626e333948d1613c
waramsey <46686986+waramsey@users.noreply.github.com>
2019-10-19 10:36:17 -0500

    Fixed Jumping
    Integrated and updated Ryan's script for player movement.  Jumping now only happens when player is touching the ground.

--- SpoopyJamProject/Assets/Scripts/PlayerMovementScript.cs
+++ SpoopyJamProject/Assets/Scripts/PlayerMovementScript.cs
@@ -6,15 +6,28 @@ public class PlayerMovementScript : MonoBehaviour
 {
     public Rigidbody2D rb;
 	public Vector2 sidewaysForce;
+    public GameObject[] ground;
 
 	void start()
 	{
 		rb = GetComponent<Rigidbody2D>();
-	}
+    }
 	
 	// Update is called once per frame
     void Update()
     {
+        if (Input.GetKey("space"))
+        {
+            ground = GameObject.FindGameObjectsWithTag("ground"); //Might cause problems refilling array later
+            for (int i = 0; i < ground.Length; i++)
+            {
+                if (GetComponent<BoxCollider2D>().IsTouching(ground[i].GetComponent<BoxCollider2D>()))
+                {
+                    rb.velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, 5);
+                }
+            }
+        }
+
         if (Input.GetKey("d"))
 		{
 			rb.velocity = new Vector2(2, GetComponent<Rigidbody2D>().velocity.y);
