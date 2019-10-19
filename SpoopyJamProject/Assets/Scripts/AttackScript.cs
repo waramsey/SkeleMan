d0fbb0d3a2b79e5be1b1049b267269fb6504296c
Narducky <47222373+Narducky@users.noreply.github.com>
2019-10-19 11:42:41 -0500

    Added BoneThrowing
    Can now ranged attack, added commented out code for damage, sprite on impact, and destroying.

---
+++ SpoopyJamProject/Assets/Scripts/AttackScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackScript : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bonePrefab;
+    public float fireRate = 1f;
+    private float lastShot = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("enter"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        if (Time.time > fireRate + lastShot)
+        {
+            Instantiate(bonePrefab, firePoint.position, firePoint.rotation);
+            lastShot = Time.time;
+        }
+    }
+}
---
+++ SpoopyJamProject/Assets/Scripts/ThrownBone.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrownBone : MonoBehaviour
+{
+    public float speed = 20f;
+    public Rigidbody2D rb;
+    public GameObject[] enemies;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        Debug.Log(hitInfo.name);
+
+        /*
+        Enemy enemy = hitInfo.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
+
+        Instantiate(impactEffect, transform.position, transform.rotation);
+        */
+        Destroy(gameObject);
+    }
+}
