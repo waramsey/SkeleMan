add2df18157b2894e0c7a5c09aa41d51798d07a9
Narducky <duckynar@gmail.com>
2019-10-19 00:17:36 -0500

    Added Movement
    Need to debug sliding

---
+++ SpoopyJamProject/Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+    public Rigidbody2D rb;
+	public Vector2 sidewaysForce;
+
+	void start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("d"))
+		{
+			rb.velocity = new Vector2(2, GetComponent<Rigidbody2D>().velocity.y);
+		}
+
+		if (Input.GetKey("a"))
+		{
+			rb.velocity = new Vector2(-2, GetComponent<Rigidbody2D>().velocity.y);
+		}
+    }
+}
